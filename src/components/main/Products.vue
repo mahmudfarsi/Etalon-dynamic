d20fb4441e087089fff286bfac91c3a08c63bdf2
mahmud farsi <mhfarsi002022@gmail.com>
2024-03-13 15:21:29 +0330

    add max and min width in tailwind-config

--- src/components/main/Products.vue
+++ src/components/main/Products.vue
@@ -1,5 +1,5 @@
 <template>
-  <Card class="w-[303px] min-w-[300px]">
+  <Card class="w-303 min-w-300">
     <template #header>
         <div
           class="bg-white rounded-2xl overflow-hidden flex flex-col items-center gap-[10px] px-[17px] pb-[22px] pt-[10px]"
