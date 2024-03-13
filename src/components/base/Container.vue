38d96006f98038f41f0458415a5e7e960212c0cc
mahmud farsi <mhfarsi002022@gmail.com>
2024-03-13 16:44:55 +0330

    edit width and max-width in other components

--- src/components/base/Container.vue
+++ src/components/base/Container.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class=" container smm:w-full mx-auto  md:w-[900px] md:min-w-[900px]
-         lg:w-[1120px]  xl:w-[1320px]">
+    <div class=" container smm:w-full mx-auto  md:w-900p md:min-w-900
+         lg:w-1120  xl:w-1320">
         <slot/>
     </div>
 </template>
--- src/components/base/Header.vue
+++ src/components/base/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="w-fullh-[40px] smm:hidden md:flex lg:items-center">
+    <header class="w-full h-40px smm:hidden md:flex lg:items-center">
         <slot/>
     </header>
 </template>
